445ddba6e3c6e35fd8f47295040c75853c54d423
象征 <hehongbo3986907440@163.com>
2019-09-09 17:42:52 +0800

    处理器

---
+++ src/main/java/com/study/netty/wechat/WeCharInitHandler.java
@@ -0,0 +1,19 @@
+package com.study.netty.wechat;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class WeCharInitHandler extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new StringDecoder());
+        pipeline.addLast(new StringEncoder());
+        pipeline.addLast(new MyWeCharHandler());
+    }
+}
